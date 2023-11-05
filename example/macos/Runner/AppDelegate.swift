0f343e33949a51fbad295a7887499e4ea5cc7623
Tong Mu <dkwingsmt@gmail.com>
2023-11-05 01:20:35 -0700

    multiview workable

--- example/macos/Runner/AppDelegate.swift
+++ example/macos/Runner/AppDelegate.swift
@@ -1,9 +1,28 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import Cocoa
 import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+  lazy var flutterEngine = FlutterEngine(name: "io.flutter", project: nil, allowHeadlessExecution: true)
+
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
+  }
+
+  override func applicationDidFinishLaunching(_ aNotification: Notification) {
+    flutterEngine.run(withEntrypoint: nil)
+    RegisterGeneratedPlugins(registry: flutterEngine)
+
+    let mainWindow = self.mainFlutterWindow as! MainFlutterWindow
+    mainWindow.showFlutter(engine: flutterEngine)
+
+    let newWindow = SideFlutterWindow(engine: self.flutterEngine)
+    DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+      let newWindow = SideFlutterWindow(engine: self.flutterEngine)
+    }
   }
 }
--- example/macos/Runner/MainFlutterWindow.swift
+++ example/macos/Runner/MainFlutterWindow.swift
@@ -1,15 +1,48 @@
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import Cocoa
 import FlutterMacOS
 
+func windowOrigin(viewId: Int64) -> CGPoint {
+  let screen = NSScreen.screens[min(2, NSScreen.screens.count - 1)]
+  let topLeft = screen.visibleFrame.origin
+  let x = CGFloat(viewId) * 300.0 + topLeft.x + 300.0
+  let y = CGFloat(viewId) * 200.0 + topLeft.y + 1500.0
+  return CGPoint.init(x: x, y: y)
+}
+
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController.init()
+  func showFlutter(engine: FlutterEngine) {
+    let flutterViewController = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
     let windowFrame = self.frame
+
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.setFrameTopLeftPoint(windowOrigin(viewId: 0))
+  }
+}
 
-    RegisterGeneratedPlugins(registry: flutterViewController)
+class SideFlutterWindow: NSWindow {
+  init(engine: FlutterEngine) {
+    super.init(contentRect: .init(origin: .zero,
+                                  size: .init(width: 400,
+                                              height: 400)),
+               styleMask: [.titled, .closable, .miniaturizable, .resizable],
+               backing: .buffered,
+               defer: false)
+    let windowFrame = self.frame
+    self.contentViewController  = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
+    self.setFrame(windowFrame, display: true)
+    self.makeKeyAndOrderFront(nil)
+    self.title = String(format: "Flutter Window #%llu", windowId())
+    self.isReleasedWhenClosed = false
 
-    super.awakeFromNib()
+    self.setFrameTopLeftPoint(windowOrigin(viewId: windowId()))
+  }
+
+  func windowId() -> Int64 {
+    return (self.contentViewController as! FlutterViewController).viewId
   }
 }
